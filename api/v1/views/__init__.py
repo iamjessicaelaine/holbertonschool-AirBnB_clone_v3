632e8a893e4d62d345232db5fff922b600bdfc0c
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-28 13:32:45 -0700

    correcting typo in views module initialization file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """treat my views directory as a module & execute this code when is loaded"""
 
 from flask import Blueprint
-from api.v1.view.index import *
+from api.v1.views.index import *
 
 # create a Blueprint for app_views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')

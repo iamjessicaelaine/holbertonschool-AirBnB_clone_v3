2f8f5ee02df272c9334ff2c2608c53825e74b560
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-28 20:31:36 -0700

    handled 404 error routing for api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """start flask, register a blueprint and define status of API route"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,6 +17,14 @@ app.register_blueprint(app_views)
 def teardown_storage(self):
     """call functions to be executed when app context ends"""
     storage.close()
+
+
+# create handler for 404 errors
+@app.errorhandler(404)  # 404 error is being handled and treated as e
+def not_found(e):
+    """define response for 404 errors suitable for api"""
+    response = {"error": "Not found"}
+    return jsonify(response), 404  # set the error status explicitly
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')

19066ce798b7ae77ce34ec7a0ba1d7b75c821901
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-28 19:22:05 -0700

    add route for stats to count obj by types

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""define /status route  on the object app_views"""
+"""define routes for the app_views blueprint"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+
 
 # an endpoint that retieves status of the API
 @app_views.route('/status', strict_slashes=False)
@@ -11,10 +12,13 @@ def status():
     api_status = {"status": "OK"}
     return jsonify(api_status)
 
+
 # creating an endpoin that retrieves the number of each obj by type
-@app_views.route('/api/v1/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """retrieves the number of each object by type"""
-    for each object in storage:
-        run the count method on it
-        print the count...or added to the dictionary to be jsonified
+    objcounts = {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
+                 'reviews': 'Review', 'states': 'State', 'users': 'User'}
+    for key, value in objcounts.items():
+        objcounts[key] = storage.count(value)
+    return jsonify(objcounts)

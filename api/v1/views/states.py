e94ef5b822b7b3abe2bbfd4e5ba84de55fcd3ce3
Jessica Loyd <jessicaelaineloyd@gmail.com>
2022-07-29 12:14:57 -0700

    testing removal of strct slashes from all states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'])
 def allstates():
     """retrieves list of all stte objects"""
     statedict = (storage.all(State).values())
